bd22f83522c6581016e1cd2489c1bae3ca2ce60b
elmarce23 <andresmarcelo55@gmail.com>
2023-06-19 13:01:25 -0600

    IU terminada

--- app/src/main/java/com/example/smartplace/MainActivity.java
+++ app/src/main/java/com/example/smartplace/MainActivity.java
@@ -1,14 +1,33 @@
 package com.example.smartplace;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
+import android.app.FragmentTransaction;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class MainActivity extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setDelay(800);
     }
+
+    private void setDelay(int Delay){
+        final Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Fragment content = new MainFrame();
+                FragmentTransaction transaction = getFragmentManager().beginTransaction();
+                //transaction.replace(R.id.FCV1, content).commit();
+            }
+        }, Delay);
+    }
+
 }
