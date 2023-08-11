1cf5566aa5d8a32a7e40b7404f70db98807f887f
elmarce23 <andresmarcelo55@gmail.com>
2023-08-10 18:18:44 -0600

    Bluettoh conectado, falta que envie datos. Hacer Tests

---
+++ app/src/main/java/com/example/smartplace/Dispositivos.java
@@ -0,0 +1,114 @@
+package com.example.smartplace;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
+import android.Manifest;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.Set;
+
+public class Dispositivos extends AppCompatActivity {
+
+    //LOGCAT
+    private static final String TAG = "Dispositivos";
+    //Declara la lista
+    ListView lista;
+    public static String EXTRA_DEVICE_ADDRESS = "device_address";
+
+    //Bluetooth
+    private BluetoothAdapter mBtAdapter;
+    private ArrayAdapter arrayAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dispositivos);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        verificarEstadoBT();
+
+        //Inicializa el List
+        arrayAdapter = new ArrayAdapter(this, R.layout.activity_dispositivos);
+        lista = (ListView) findViewById(R.id.listaDis);
+        lista.setAdapter(arrayAdapter);
+        lista.setOnClickListener((View.OnClickListener) mclickListener);
+
+        mBtAdapter = BluetoothAdapter.getDefaultAdapter();
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.BLUETOOTH_CONNECT) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        Set<BluetoothDevice> pairedDevices = mBtAdapter.getBondedDevices();
+
+        if(pairedDevices.size() > 0){
+            for(BluetoothDevice device : pairedDevices)
+                arrayAdapter.add(device.getName() + "\n" + device.getAddress());
+        }
+
+    }
+
+    private AdapterView.OnItemClickListener mclickListener = new AdapterView.OnItemClickListener(){
+        @Override
+        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+            String info = ((TextView) view).getText().toString();
+            String address = info.substring(info.length() - 17);
+
+            finishAffinity();
+
+            Intent intent = new Intent(Dispositivos.this, MenuActivity.class);
+            intent.putExtra(EXTRA_DEVICE_ADDRESS, address);
+            startActivity(intent);
+
+        }
+    };
+
+    private void verificarEstadoBT() {
+        //Comprueba BT Activo
+        mBtAdapter = BluetoothAdapter.getDefaultAdapter();
+        if (mBtAdapter == null) {
+            Toast.makeText(getBaseContext(), "No Bluetooth", Toast.LENGTH_LONG).show();
+        } else {
+            if (mBtAdapter.isEnabled()) {
+                Log.d(TAG, "Activated");
+            } else {
+                //Solicita activar Bluetooth
+                Intent enableIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+                if (ActivityCompat.checkSelfPermission(this, Manifest.permission.BLUETOOTH_CONNECT) != PackageManager.PERMISSION_GRANTED) {
+                    // TODO: Consider calling
+                    //    ActivityCompat#requestPermissions
+                    // here to request the missing permissions, and then overriding
+                    //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                    //                                          int[] grantResults)
+                    // to handle the case where the user grants the permission. See the documentation
+                    // for ActivityCompat#requestPermissions for more details.
+                    return;
+                }
+                startActivityForResult(enableIntent, 1);
+            }
+        }
+
+    }
+}
--- app/src/main/java/com/example/smartplace/MenuActivity.java
+++ app/src/main/java/com/example/smartplace/MenuActivity.java
@@ -3,8 +3,16 @@ package com.example.smartplace;
 import static android.content.ContentValues.TAG;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothSocket;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.os.Handler;
 import android.util.Log;
 import android.view.View;
 import android.widget.CompoundButton;
@@ -14,6 +22,11 @@ import android.widget.Toast;
 
 import com.example.smartplace.data.Dispositivo;
 import com.example.smartplace.remote.ApiService;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.UUID;
 
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -27,6 +40,15 @@ public class MenuActivity extends AppCompatActivity {
     private TextView mResponseTv;
     private ApiService mAPIService;
 
+    Handler bluetoothIn;
+    final int handlerState = 0;
+    private BluetoothAdapter bluetoothAdapter = null;
+    private BluetoothSocket bluetoothSocket = null;
+    private StringBuilder DataString = new StringBuilder();
+    private ConnectedThread conexion;
+    private static final UUID BTMODULE =  UUID.fromString("00001101-0000-1000-8000-00805F9B34FB");
+    private static String address = null;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -35,6 +57,17 @@ public class MenuActivity extends AppCompatActivity {
 
         //connector = new API_Connector();
         initComponents();
+
+        bluetoothIn = new Handler() {
+            public void handleMessage(android.os.Message msg) {
+                if (msg.what == handlerState) {
+                    //Interacción con los datos de ingreso
+                }
+            }
+        };
+
+        bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        VerificarEstadoBT();
 
         c1.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
@@ -47,7 +80,7 @@ public class MenuActivity extends AppCompatActivity {
         c2.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                creaMensaje("1", isChecked);
+                creaMensaje("2", isChecked);
                 //connector.putEstado(2); // Debe reemplazar a isChecked
             }
         });
@@ -55,7 +88,7 @@ public class MenuActivity extends AppCompatActivity {
         c3.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                creaMensaje("1", isChecked);
+                creaMensaje("3", isChecked);
                 //connector.putEstado(2); // Debe reemplazar a isChecked
             }
         });
@@ -63,7 +96,7 @@ public class MenuActivity extends AppCompatActivity {
         c4.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                creaMensaje("1", isChecked);
+                creaMensaje("4", isChecked);
                 //connector.putEstado(2); // Debe reemplazar a isChecked
             }
         });
@@ -79,10 +112,11 @@ public class MenuActivity extends AppCompatActivity {
 
     protected void creaMensaje(String id, boolean act){
         String cat = act ? "Encendido" : "Apagado";
-        putDis(id, cat);
+        //putDis(id, cat);
         Toast.makeText(getApplicationContext(), "Dispositivo:name " + cat, Toast.LENGTH_LONG).show();
     }
 
+    /* Conexion API */
     public void putDis(String id, String edo){
         edo = edo.equals("Encendido") ? "1" : "0";
         mAPIService.putDis(id, edo).enqueue(new Callback<Dispositivo>() {
@@ -119,6 +153,138 @@ public class MenuActivity extends AppCompatActivity {
             mResponseTv.setVisibility(View.VISIBLE);
         }
         mResponseTv.setText(response);
+    }
+
+    /* Fin conexion API */
+
+    private BluetoothSocket createBluetoothSocket(BluetoothDevice device) throws IOException {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.BLUETOOTH_CONNECT) != PackageManager.PERMISSION_GRANTED) {
+        }
+        return device.createRfcommSocketToServiceRecord(BTMODULE);
+        //creates secure outgoing connecetion with BT device using UUID
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+
+        Intent intent = getIntent();
+        address = intent.getStringExtra(Dispositivos.EXTRA_DEVICE_ADDRESS);
+        //Setea la direccion MAC
+        BluetoothDevice device = bluetoothAdapter.getRemoteDevice(address);
+
+        try {
+            bluetoothSocket = createBluetoothSocket(device);
+        } catch (IOException e) {
+            Toast.makeText(getBaseContext(), "La creacción del Socket fallo", Toast.LENGTH_LONG).show();
+        }
+        // Establece la conexión con el socket Bluetooth.
+        try {
+            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.BLUETOOTH_CONNECT) != PackageManager.PERMISSION_GRANTED) {
+                // TODO: Consider calling
+                //    ActivityCompat#requestPermissions
+                // here to request the missing permissions, and then overriding
+                //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                //                                          int[] grantResults)
+                // to handle the case where the user grants the permission. See the documentation
+                // for ActivityCompat#requestPermissions for more details.
+                bluetoothSocket.connect();
+                //Toast.makeText(getBaseContext(), "CONEXION EXITOSA", Toast.LENGTH_SHORT).show();
+
+                //return;
+            }
+
+            //btSocket.connect();
+        } catch (IOException e) {
+            try {
+                bluetoothSocket.close();
+            } catch (IOException e2) {
+            }
+        }
+        conexion = new ConnectedThread(bluetoothSocket);
+        conexion.start();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        try { // Cuando se sale de la aplicación esta parte permite que no se deje abierto el socket
+            bluetoothSocket.close();
+        } catch (IOException e2) {
+        }
+    }
+
+    //Comprueba que el dispositivo Bluetooth
+    //está disponible y solicita que se active si está desactivado
+    private void VerificarEstadoBT() {
+        if (bluetoothAdapter == null) {
+            Toast.makeText(getBaseContext(), "El dispositivo no soporta bluetooth", Toast.LENGTH_LONG).show();
+        } else {
+            if (bluetoothAdapter.isEnabled()) {
+            } else {
+                Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+                if (ActivityCompat.checkSelfPermission(this, Manifest.permission.BLUETOOTH_CONNECT) != PackageManager.PERMISSION_GRANTED) {
+                    // TODO: Consider calling
+                    //    ActivityCompat#requestPermissions
+                    // here to request the missing permissions, and then overriding
+                    //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                    //                                          int[] grantResults)
+                    // to handle the case where the user grants the permission. See the documentation
+                    // for ActivityCompat#requestPermissions for more details.
+                    startActivityForResult(enableBtIntent, 1);
+                    //return;
+                }
+
+            }
+        }
+    }
+
+    //Crea la clase que permite crear el evento de conexion
+    private class ConnectedThread extends Thread {
+        private final InputStream mmInStream;
+        private final OutputStream mmOutStream;
+
+        public ConnectedThread(BluetoothSocket socket)
+        {
+            InputStream tmpIn = null;
+            OutputStream tmpOut = null;
+            try
+            {
+                tmpIn = socket.getInputStream();
+                tmpOut = socket.getOutputStream();
+            } catch (IOException e) { }
+            mmInStream = tmpIn;
+            mmOutStream = tmpOut;
+        }
+
+        public void run()
+        {
+            byte[] byte_in = new byte[1];
+            // Se mantiene en modo escucha para determinar el ingreso de datos
+            while (true) {
+                try {
+                    mmInStream.read(byte_in);
+                    char ch = (char) byte_in[0];
+                    bluetoothIn.obtainMessage(handlerState, ch).sendToTarget();
+                } catch (IOException e) {
+                    break;
+                }
+            }
+        }
+
+        //Envio de trama
+        public void write(String input)
+        {
+            try {
+                mmOutStream.write(input.getBytes());
+            }
+            catch (IOException e)
+            {
+                //si no es posible enviar datos se cierra la conexión
+                Toast.makeText(getBaseContext(), "La Conexión fallo", Toast.LENGTH_LONG).show();
+                finish();
+            }
+        }
     }
 
 }
